fc5444c730a32266137ee5aa2fcb6b3b97995845
erokree <as6858732@gmail.com>
2022-07-15 12:12:32 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heading from './Heading'
+import Navbar from './Navbar'
+const Header = () => {
+  return (
+    <div><Heading />
+    <Navbar />
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tabs from './Tabs'
+const Navbar = () => {
+  return (
+    <div>
+    <Tabs />
+    </div>
+
+  )
+}
+
+export default Navbar
